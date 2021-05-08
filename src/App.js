bcb37771c025672124eb56d82076e4154317a9d5
Viragi Shah <viragi311@gmail.com>
2021-05-07 22:39:16 -0700

    Handled search, used axios for quering  github search api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './App.css';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from 'axios';
 import DisplayUserNames from './components/DisplayUserNames';
+import PaginationComponent from './components/PaginationComponent';
 
+
+const USER_PER_PAGE = 10;
 class App extends React.Component{
   constructor(props){
     super(props)
@@ -19,6 +23,16 @@ class App extends React.Component{
       searchTerm: e.target.value
     })
   }
+  handleSearch = async () => {
+
+    if (this.state.searchTerm.trim().length == 0){
+      console.log("In if");
+      return
+    }
+    console.log("here");
+    let res = await axios.get(`https://api.github.com/search/users?q=${this.state.searchTerm}&per_page=${USER_PER_PAGE}&page=3`);
+    console.log(res, "res here");
+  }
 
   render(){
     return (
@@ -31,9 +45,10 @@ class App extends React.Component{
                 value = {this.state.searchTerm}
                 required
               />
-              <Button as="input" type="submit" value="Submit" />
+              <Button as="input"  value="Search" onClick = {this.handleSearch}/>
         </Form>
         <DisplayUserNames/>
+        <PaginationComponent/>
       </div>
     );
   }
