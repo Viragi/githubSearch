f80ec4985b0b78b4e5c500b7fea47161152bfaf8
Viragi Shah <viragi311@gmail.com>
2021-05-07 22:12:19 -0700

    Added Form to search User Name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React from 'react';
 import './App.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import DisplayUserNames from './components/DisplayUserNames';
 
-function App() {
-  return (
-    <div className="App">
-    Hello
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      searchTerm : "",
+      userNameList : []
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      ...this.state,
+      searchTerm: e.target.value
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Form>
+          <Form.Control
+                type="text"
+                placeholder="Search Github UserName"
+                onChange = {this.handleInput}
+                value = {this.state.searchTerm}
+                required
+              />
+              <Button as="input" type="submit" value="Submit" />
+        </Form>
+        <DisplayUserNames/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DisplayUserNames.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DisplayUserNames(){
+    return(
+        <div>
+            Usernames here
+        </div>
+    )
+
+}
