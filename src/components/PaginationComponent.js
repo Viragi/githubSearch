bc081173395e7f2d98e8026b22255230d900b503
Viragi Shah <viragi311@gmail.com>
2021-05-09 01:21:18 -0700

    Changed Logic of pagination

--- src/components/PaginationComponent.js
+++ src/components/PaginationComponent.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import {React,useState} from 'react';
 import Pagination from "react-bootstrap/Pagination";
-export default function PaginationComponent(props){
 
-    let totalCount = props.totalCount;
+const USER_PER_PAGE = 20;
+export default function PaginationComponent(props){
+    const [startPageNo, setStartPageNo] = useState(1)
+    let totalpages = Math.floor(1000/USER_PER_PAGE);
     let activePage = props.activePage;
     
+    const handlePageNum  = (dir) =>{
+        if (dir == 'left') {
+            if (startPageNo == 1) return
+            setStartPageNo(startPageNo-10)
+        } else {
+            if (startPageNo+10 > totalpages) return
+            setStartPageNo(startPageNo + 10)
+        }
+    }
     const handleClick = (arr) => {
        props.handleActivePage(arr);
     }
     
     let reqComp = new Array(10).fill(1).map((item,i)=>{
-        return( <Pagination.Item active = {activePage == i+1} key = {`${i}pg`} onClick = {(e)=>handleClick([0,e.target.innerText,0])}> {i+1} </Pagination.Item>)
+        return( <Pagination.Item active = {activePage == i+1} key = {`${i}pg`} onClick = {(e)=>handleClick(e.target.innerText)}> {startPageNo+i} </Pagination.Item>)
     });
     
     return(
         <Pagination>
-            <Pagination.Prev onClick = {()=>handleClick([1,0,0])}/>
+            <Pagination.First onClick = {()=>handlePageNum("left")}/>
                 {reqComp}
-            <Pagination.Ellipsis />
-            <Pagination.Item>{totalCount}</Pagination.Item>
-            <Pagination.Next onClick = {()=>handleClick([0,0,1])}/>
+            <Pagination.Last onClick = {()=>handlePageNum("right")}/>
         </Pagination>
     )
 }
